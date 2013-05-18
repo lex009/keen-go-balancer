61b48d6e815a5e27e3a36c1fc22429d25f7966f3
Alex Belyaev <a.v.belyaev@gmail.com>
2013-05-18 16:56:39 +0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: alexbelyaev
+ * Date: 18.05.13
+ * Time: 16:10
+ * To change this template use File | Settings | File Templates.
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello world!")
+
+}
